0acf36cf5becf9cb86b77e1b173908b04b9a7ca7
cdzhuzhiyong <cdzhuzhiyong>
2018-09-08 09:37:28 +0800

    fix:

--- src/main/java/edu/swpu/spring_mybatis/service/UserService.java
+++ src/main/java/edu/swpu/spring_mybatis/service/UserService.java
@@ -4,5 +4,6 @@ import edu.swpu.spring_mybatis.bean.User;
 import edu.swpu.spring_mybatis.service.base.BaseService;
 
 public interface UserService extends BaseService<User> {
+    //自定义查询
     public String selectAccountNum(User condition) throws Exception;
 }
